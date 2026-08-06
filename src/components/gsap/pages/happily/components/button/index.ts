0952b5e133423c4becfaf421bf049d658e306986
Rupesh Gyawali <rupeshgyawali49@gmail.com>
2026-08-06 14:46:49 +0545

    fix:updated wave button for happily

--- src/components/gsap/pages/happily/components/button/index.ts
+++ src/components/gsap/pages/happily/components/button/index.ts
@@ -1,6 +1,5 @@
 import ActionButton from "./ActionButton.astro";
 import ButtonExpanded from "./ButtonExpanded.astro";
 import ButtonWave from "./ButtonWave.astro";
-import ButtonWaveText from "./ButtonWaveText.astro";
 
-export { ButtonWaveText, ButtonWave, ButtonExpanded, ActionButton };
+export { ButtonWave, ActionButton, ButtonExpanded };
