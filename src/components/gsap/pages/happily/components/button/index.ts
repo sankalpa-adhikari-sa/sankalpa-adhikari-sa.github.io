4540537ed7725e1b09add2d8e10f9abfb925ad18
Sankalpa Adhikari <sankalpa2522@gmail.com>
2026-08-06 11:11:10 +0545

    fix:heropage

--- src/components/gsap/pages/happily/components/button/index.ts
+++ src/components/gsap/pages/happily/components/button/index.ts
@@ -1,5 +1,6 @@
+import ActionButton from "./ActionButton.astro";
 import ButtonExpanded from "./ButtonExpanded.astro";
 import ButtonWave from "./ButtonWave.astro";
 import ButtonWaveText from "./ButtonWaveText.astro";
 
-export { ButtonWaveText, ButtonWave, ButtonExpanded };
+export { ButtonWaveText, ButtonWave, ButtonExpanded, ActionButton };
