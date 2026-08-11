1babd213d7de829177175251ca9723c36d7cde59
Rupesh Gyawali <rupeshgyawali49@gmail.com>
2026-08-11 11:18:10 +0545

    feat: Added Interactive Iframe

--- src/components/gsap/components/index.ts
+++ src/components/gsap/components/index.ts
@@ -2,5 +2,12 @@ import AnimButton from "./AnimButton.astro";
 import Card from "./card.astro";
 import ImageSteppedSlider from "./ImageSteppedSlider.astro";
 import ImageTrailImpl from "./ImageTrailImpl.astro";
+import Wrap from "./wrap.astro";
 
-export { AnimButton, Card, ImageTrailImpl as ImageTrail, ImageSteppedSlider };
+export {
+	AnimButton,
+	Card,
+	ImageTrailImpl as ImageTrail,
+	ImageSteppedSlider,
+	Wrap,
+};
